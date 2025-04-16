3a3e233d2e5c23e65a18867e5115170b8f43ad9b
Deo40 <deowhaane@gmail.com>
2025-04-16 12:28:42 +0200

    node modules installed

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'assets_app',
-  password: process.env.DB_PASSWORD || 'yourpassword',
+  database: process.env.DB_NAME || 'Assets',
+  password: process.env.DB_PASSWORD || 'Kurtley@1998',
   port: process.env.DB_PORT || 5432,
 });
 

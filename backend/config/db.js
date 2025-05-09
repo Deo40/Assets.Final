15758d76124f7ff70377626c4ec618a89e6ce102
Deo40 <deowhaane@gmail.com>
2025-05-09 11:53:01 +0200

    db config updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'avnadmin',
+  host: process.env.DB_HOST || 'pg-38b8ccb-mern-stack-app.k.aivencloud.com',
   database: process.env.DB_NAME || 'Assets',
-  password: process.env.DB_PASSWORD || 'Kurtley@1998',
-  port: process.env.DB_PORT || 5432,
+  password: process.env.DB_PASSWORD || 'AVNS_zYvgxyJ0IK_VTTNDnPv',
+  port: process.env.DB_PORT || 20960,
+  ssl: {
+    rejectUnauthorized: false},
 });
 
 module.exports = pool;
